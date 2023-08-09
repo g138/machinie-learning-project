02065e1eb8a41f9edc1c8c4f8a4ab1828e873a3e
Gaurav Gupta <36992519+g138@users.noreply.github.com>
2023-08-09 16:16:07 +0100

    EDA and problem statement.

--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,8 @@
 from distutils.log import INFO
 import logging
 import os
-from datetime import date, datetime
+from datetime import  datetime
+
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
